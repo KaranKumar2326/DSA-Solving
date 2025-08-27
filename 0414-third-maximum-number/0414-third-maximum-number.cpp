d6067fc4850e4f9ab2ce01e336160535da1b71f4
Karan Kumar <127759113+KaranKumar2326@users.noreply.github.com>
2025-08-28 02:43:16 +0530

    Time: 0 ms (100%), Space: 12.9 MB (55.59%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-      long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+     long  first = LONG_MIN;
+     long  second = LONG_MIN;
+     long   third = LONG_MIN;
 
-    for (int num : nums) {
-        // Skip if num is already one of the three largest distinct numbers
-        if (num == first || num == second || num == third) {
+     for(int num : nums){
+        if(num==first||num==second||num==third){
             continue;
         }
-
-        // Update the largest three numbers
-        if (num > first) {
+        if(num>first){
             third = second;
             second = first;
             first = num;
-        } else if (num > second) {
+        }
+        else if(num>second){
             third = second;
             second = num;
-        } else if (num > third) {
+        }
+        else if(num>third){
             third = num;
         }
-    }
+     }
 
-    // If the third number doesn't exist, return the largest (first)
-    return third == LONG_MIN ? first : third;
 
+     if(third==LONG_MIN) return first;
+
+     return third;
 
     }
 };
