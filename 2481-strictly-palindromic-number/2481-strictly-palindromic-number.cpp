d92f7b03555baf4d387142be6d38d30ad291fcde
Karan Kumar <127759113+KaranKumar2326@users.noreply.github.com>
2025-08-30 18:56:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.47%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -1,29 +1,6 @@
 class Solution {
-private:
-    bool isPalindromic(string r) {
-        string s = r;
-        reverse(r.begin(), r.end());
-        return s == r;
-    }
-
-private:
-    string convertBase(int n, int base) {
-        string out;
-        while (n > 0) {
-            if (n % base != 0) out += to_string(n % base);
-            else out += "0";
-            n /= base;
-        }
-        return out;
-    }
-
 public:
     bool isStrictlyPalindromic(int n) {
-        string s;
-        for (int i = 2; i <= n-2; i++) {
-            s = convertBase(n, i);
-            if (!isPalindromic(s)) return false;
-        }
-        return true;
+        return false;
     }
 };
