4acabd663f749c919bd1ef0332480adcf4e82ca7
Karan Kumar <127759113+KaranKumar2326@users.noreply.github.com>
2025-10-24 18:05:04 +0530

    Time: 3 ms (83.85%), Space: 10.1 MB (45.13%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        string prev = countAndSay(n - 1);
+        string res = "";
+        
+        int count = 1;
+        for (int i = 1; i <= prev.size(); i++) {
+            if (i < prev.size() && prev[i] == prev[i - 1]) {
+                count++;
+            } else {
+                res += to_string(count);
+                res += prev[i - 1];
+                count = 1;
+            }
+        }
+        return res;
+    }
+};
